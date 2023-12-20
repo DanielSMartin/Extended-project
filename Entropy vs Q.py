bda81a689a811703d87249950824c47db72eb46c
DanielSMartin <150593385+DanielSMartin@users.noreply.github.com>
2023-12-20 19:29:54 +0000

    Update Entropy vs Q.py

--- Entropy vs Q.py
+++ Entropy vs Q.py
@@ -49,7 +49,42 @@ plt.ylabel('$\Delta S$',fontsize=18)
 plt.xlabel('$Q$',fontsize=18)
 plt.legend(fontsize=10) 
 
+
 '''
 Slight difference to the graph in the Yanik paper, but can be reproduced for a middle z0 value of arounf -0.35. The overall pattern of entropy holds
 but not the exact same graph visual 
+'''
+
+#%% Plotting second entropy plot to show the cancellation above certain G value
+
+# z0 = -0.05 so constant 
+ent_z = []
+
+for i in kappa_range:
+    kappa = i
+    q = Q(kappa)
+    g = gamma(j)
+    l = length(i,-0.05)
+    
+    ent =  DS(q,g,l)
+    ent_z.append(ent)
+    
+min_ent_z = [-value for value in ent_z]
+
+
+plt.plot(Q_vals,min_ent_z, linestyle='--', color='steelblue', linewidth=2, dashes=(10, 2), label = r'$-\Delta S_M$')
+plt.plot(Q_vals,np.log(2)*np.ones([1000,1]), color='k', linewidth=2, dashes=(10,2,3,2), label = r'$\Delta S_{er}$')
+plt.plot(Q_vals,ent_z + np.log(2), linestyle='--', color='tomato', linewidth=2, dashes=(2, 1), label = r'$\Delta S$')
+
+
+plt.tick_params(axis='both', which='major', labelsize=12)
+plt.ylabel('$\Delta S$',fontsize=18)
+plt.xlabel('$Q$',fontsize=18)
+plt.legend(fontsize=10)
+
+'''
+Again there is a slight discrepency with the paper. The overall pattern can be seen and the entropys are still shown to cancel, however the value of Q at which they 
+do has decreased down to 1. This may still be proportionate to the decreased scale I have plotted with. The cause of these differences may stem from division by zero
+but I am currently unsure if there is an issue in the actual code, since the same conclusions can be drawn but for a shifted range a value might differ from when the 
+paper calculated these values.
 '''
