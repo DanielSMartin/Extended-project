16d66caa3e5135ea9cfe210a84e2260996445b8e
DanielSMartin <150593385+DanielSMartin@users.noreply.github.com>
2024-03-24 13:51:29 +0000

    Create Two qubits x hamiltonian  .py

---
+++ Two qubits x hamiltonian  .py
@@ -0,0 +1,117 @@
+#%% Different Hamiltonian work extraction
+
+
+y = 0 
+ET_vals = np.zeros([1000,4])
+Ef_vals = np.zeros([1000,4])
+Q_vals = np.zeros([1000,4])
+
+
+for k in [0.01,0.4]:
+    for f in [-1,1]:
+        beta = 0.75
+        ea = 0.1/beta
+        eb = 2/beta
+        x=0
+        gx_beta_vals = np.linspace(-0.5,0.5,1000)
+        gx_range = (1/beta)*np.ones(len(gx_beta_vals))*gx_beta_vals
+        for gx in gx_range:
+            
+            Ham = H_2(ea,eb,gx,0,0)
+            rhoT = rho(beta,Ham)
+            
+            M = Meas(k,1)
+            rhom = M @ rhoT @ np.conj(M).T / np.trace(M @ rhoT @ np.conj(M).T)
+            rhoam = np.trace(rhom.reshape(2,2,2,2),axis1=1,axis2=3)
+            X , Y , Z = BlochCoords(rhoam)
+            
+            if f == -1:
+                theta = -1/2 * np.arctan(X/Z) 
+            elif f == 1:
+                theta =-1/2 * np.arctan(X/Z) + np.pi/2
+                
+            U = np.kron(U_fb(theta),I)
+            Udag = np.kron(np.conj(U_fb(theta)).T,I)
+            rhof = U @ rhom @ Udag
+            
+            
+            
+            ET_vals[x,y] = Energy(rhoT,Ham)
+            Ef_vals[x,y] = Energy(rhof,Ham)
+            Q_vals[x,y] = Energy(rhoT,Ham) - Energy(rhof,Ham)
+            x += 1
+        y += 1
+
+
+#%%
+
+plt.plot(gx_beta_vals,Q_vals[:,0],linestyle='-.', color='purple',label='$k=0 , F_1 = -1$')
+plt.plot(gx_beta_vals,Q_vals[:,1], color='black',label='$k=0 , F_1 = +1$')
+plt.plot(gx_beta_vals,Q_vals[:,2],linestyle='--', color='blue',label='$k=0.4 , F_1 = -1$')
+plt.plot(gx_beta_vals,Q_vals[:,3],linestyle='dotted', color='red',label='$k=0.4 , F_1 = +1$')
+
+plt.axhline(y=0,linestyle='dashed',color='grey')
+
+
+plt.xlabel('$g_z [kT]$')
+plt.ylabel('$Q_A$')
+plt.legend()
+
+#%% Efficiency against kappa for set g vals - x ham
+
+f = -1
+beta = 0.75
+y = 0
+
+W_ext_vals = np.zeros([100,5])
+W_eras_vals = np.zeros([100,5])
+efficiency_vals = np.zeros([100,5])
+
+
+for g in [-0.2/beta,-0.1/beta,0,0.1/beta,0.2/beta]:
+    ea = 0.1/beta
+    eb = 2/beta
+    x=0
+    Ham = H_2(ea,eb,g,0,0)
+    rhoT = rho(beta,Ham)
+        
+    kappa_range = np.linspace(0.001,0.999,100)
+    for kap in kappa_range:
+        
+        M = Meas(kap,1)
+        rhom = M @ rhoT @ np.conj(M).T / np.trace(M @ rhoT @ np.conj(M).T)
+        rhoam = np.trace(rhom.reshape(2,2,2,2),axis1=1,axis2=3)
+        X , Y , Z = BlochCoords(rhoam)
+        
+        theta = -1/2 * np.arctan(X/Z)
+        U = np.kron(U_fb(theta),I)
+        Udag = np.kron(np.conj(U_fb(theta)).T,I)
+        rhof = U @ rhom @ Udag
+        rhoF = rhof/np.trace(rhof)
+        
+        W_ext = Energy(rhoT,Ham) - Energy(rhoF,Ham)
+        W_eras = 1 * (Entropy(rhoF)-Entropy(rhoT))
+        eff = (W_ext + W_eras)/Energy(rhom,Ham)
+        
+        W_ext_vals[x,y] = W_ext
+        W_eras_vals[x,y] = W_eras
+        efficiency_vals[x,y] = eff
+        
+        x += 1
+    y += 1
+
+
+#%% 
+
+plt.figure()
+plt.plot(kappa_range,efficiency_vals[:,0],label='$g_z=-0.2kT$')
+plt.plot(kappa_range,efficiency_vals[:,1],label='$g_z = -0.1kT$')
+plt.plot(kappa_range,efficiency_vals[:,2],label='$g_z = 0kT$')
+plt.plot(kappa_range,efficiency_vals[:,3],label='$g_z = 0.1kT$')
+plt.plot(kappa_range,efficiency_vals[:,4],label='$g_z = 0.2kT$')
+
+
+plt.xlabel('$\kappa$')
+plt.ylabel('$\eta$')
+
+plt.legend()
